6d227c2f0f1eca722bc20612b2838ab7018c9232
David LaJambe <dlajambe@gmail.com>
2023-09-22 20:00:31 -0400

    Add DoublyLinkedList class

--- src/linked_list.py
+++ src/linked_list.py
@@ -2,7 +2,6 @@ class Node():
     def __init__(self, d):
         self.d = d
         self.next = None
-        self.prev = None
 
 class SinglyLinkedList():
     def __init__(self):
@@ -34,7 +33,7 @@ class SinglyLinkedList():
         if self.head == None:
             raise ValueError('Cannot delete a node from an empty linked list')
         elif self.head.d == d:
-            self.head = None
+            self.head = self.head.next
             return
         
         # Case 2 - There are multiple nodes in the list
@@ -43,6 +42,78 @@ class SinglyLinkedList():
             if n.next.d == d:
                 n.next = n.next.next
                 return
+            n = n.next
+
+        raise ValueError('Node with value {} not found'.format(d))
+
+    def __str__(self):
+        if self.head == None:
+            return '[]'
+        
+        n = self.head
+        s = ['[']
+        while True:
+            s.append(str(n.d))
+            if n.next == None:
+                break
+            s.append(', ')
+            n = n.next
+        s.append(']')
+        return ''.join(s)
+
+class DoubleNode():
+    def __init__(self, d):
+        self.d = d
+        self.next = None
+        self.prev = None
+    
+class DoublyLinkedList():
+    def __init__(self):
+        self.head = None
+
+    def insert_front(self, d):
+        n = DoubleNode(d)
+        n.next = self.head
+        self.head = n
+
+        if self.head.next != None:
+            self.head.next.prev = self.head
+
+    def insert_back(self, d):
+        if self.head == None:
+            self.insert_front(d)
+            return
+        
+        n_prev = self.head
+        n = self.head.next
+        while n != None:
+            n_prev = n
+            n = n.next
+    
+        n = DoubleNode(d)
+        n.prev = n_prev
+        n.prev.next = n
+
+
+    def delete(self, d):
+        # Case 1 - There is one or zero nodes in the list
+        if self.head == None:
+            raise ValueError('Cannot delete a node from an empty linked list')
+        elif self.head.d == d:
+            self.head = self.head.next
+            self.head.prev = None
+            return
+        
+        # Case 2 - There are multiple nodes in the list
+        n_prev = self.head
+        n = self.head.next
+        while n != None:
+            if n.d == d:
+                n_prev.next = n.next
+                if n.next != None:
+                    n_prev.next.prev = n_prev
+                return
+            n_prev = n
             n = n.next
 
         raise ValueError('Node with value {} not found'.format(d))
